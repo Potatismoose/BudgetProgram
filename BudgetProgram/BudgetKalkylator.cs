3ec54c962a03d6d5a49ce2a269570fa4d421985e
Benny Christensen <benny@potatismoose.com>
2021-05-11 14:42:38 +0200

    Add project files.

---
+++ BudgetProgram/BudgetKalkylator.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BudgetProgram
+{
+    public class BudgetKalkylator
+    {
+    }
+}
---
+++ BudgetProgramTests/BudgetKalkylatorTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using BudgetProgram;
+
+namespace BudgetProgramTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
