6e6f76c4162bf853706baf63ee59d8c04978d346
Håkan Eriksson <badfish.paint@gmail.com>
2021-05-14 19:31:30 +0200

    Namnändring

--- BudgetProgram/HelperMethods/ExpenseHelper.cs
+++ BudgetProgram/HelperMethods/ExpenseHelper.cs
@@ -6,7 +6,7 @@ namespace BudgetProgram.HelperMethods
 {
     class ExpenseHelper
     {
-        public decimal Totalexpences(Dictionary<string, decimal> expenses)
+        public decimal TotalExpences(Dictionary<string, decimal> expenses)
         {
             decimal total = 0;
             foreach (var item in expenses.Values)
