2447371c55dc80820ab41defaf785d956ea1ec19
Håkan Eriksson <badfish.paint@gmail.com>
2021-05-26 21:12:15 +0200

    Added XML to CalculateIncomesTest_1.

--- BudgetProgramTests/BudgetKalkylator/CalculateIncomesTests.cs
+++ BudgetProgramTests/BudgetKalkylator/CalculateIncomesTests.cs
@@ -12,11 +12,18 @@ namespace BudgetProgram.BudgetKalkylator.Tests
     [TestFixture()]
     public class CalculateIncomesTests
     {
-        [TestCase(20000, 20000, TestName = "CalculateIncomes_Test_1_AddingIncomes_ReturnsBalance")]
-        public void CalculateIncomesTest(Decimal value, Decimal expected)
+        /// <summary>
+        /// Tests that the balance + income returns the new balance correctly.
+        /// The new Balance should be income + 1500.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="expected"></param>
+        [TestCase(20000, 21500, TestName = "CalculateIncomes_Test_1_AddingIncomes_ReturnsBalance")]
+        [TestCase(25000, 26500, TestName = "CalculateIncomes_Test_2_AddingIncomes_ReturnsBalance")]
+        public void CalculateIncomesTest_1(Decimal value, Decimal expected)
         {
             // Arrange 
-            decimal balance = 0;
+            decimal balance = 1500;
             var incomes = new Income();
             incomes.HouseholdIncomes = new Dictionary<string, decimal>();
             incomes.HouseholdIncomes.Add("Lön", value);
