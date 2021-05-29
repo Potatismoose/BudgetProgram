71b623ced137ce23a7c896eedfd8cbea5a5b05bd
Håkan Eriksson <badfish.paint@gmail.com>
2021-05-29 15:10:12 +0200

    Addded test for totalExpencesTests.

---
+++ BudgetProgramTests/HelperMethods/TotalExpencesTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using BudgetProgram.HelperMethods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetProgram.HelperMethods.Tests
+{
+    [TestFixture()]
+    public class TotalExpencesTests
+    {
+        [Test()]
+        public void TotalExpencesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
